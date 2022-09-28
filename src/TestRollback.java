e1e887933f875388a7fa52c12b16d13a5e8e6df8
salvatore.catania@govegans-at-904327189634 <salvatore.catania@govegans.it>
2022-09-28 10:08:58 +0200

    Revert "mul function"
    This reverts commit 70a546aa

--- src/TestRollback.java
+++ src/TestRollback.java
@@ -4,12 +4,4 @@ public class TestRollback {
         return a + b;
     }
 
-    private int mul(int a, int b) {
-        return a * b;
-    }
-
-    private int div(int a, int b) {
-        return a / b;
-    }
-
 }
